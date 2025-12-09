99aa2c1828d2ad4a7c49584354c692d3cf52cc4b
SoumithVardhan <soumithvardhan@gmail.com>
2025-12-09 21:25:23 +0530

    practice question on leetcode/codeforces added

---
+++ Practice/LeetCode/Math/1021. Remove Outermost Parentheses.cpp
@@ -0,0 +1,23 @@
+/*
+    problem link: https://leetcode.com/problems/remove-outermost-parentheses/description/
+*/
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int cnt=0;
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                if(cnt>0){
+                    ans+='(';
+                }
+                cnt++;
+            }else{
+                cnt--;
+                if(cnt>0){
+                    ans+=')';
+                }
+            }
+        }return ans;
+    }
+};
