c4655418f31d91df967e1a1bfc73e7d554741e0d
SoumithVardhan <soumithvardhan@gmail.com>
2026-03-03 18:54:34 +0530

    Add implementations for Remove Duplicates from Sorted Array, Missing Number, Move Zeros to End, and Rotate Array by K elements algorithms

---
+++ Practice/Striver A2Z/Arrays/26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0,j=0;
+        while(j<nums.size()){
+
+            while(j < nums.size() && nums[i]==nums[j]){
+                j++;
+            }
+            i++;
+            if(i<nums.size() && j<nums.size())
+            nums[i]=nums[j];
+        }
+        return i;
+    }
+};
