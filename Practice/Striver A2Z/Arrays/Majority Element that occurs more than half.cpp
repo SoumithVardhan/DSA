e5231adb46bb23dfde8ac53a1955cd1848b2ee61
SoumithVardhan <soumithvardhan@gmail.com>
2026-03-07 18:33:15 +0530

    Add implementations for Longest Subarray with Given Sum K using brute force and optimal approaches, and Majority Element that occurs more than half

---
+++ Practice/Striver A2Z/Arrays/Majority Element that occurs more than half.cpp
@@ -0,0 +1,16 @@
+//Find the Majority Element that occurs more than N/2 times
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0, count = 0;
+        for (int num : nums) {
+            if (count == 0) candidate = num;
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+};
